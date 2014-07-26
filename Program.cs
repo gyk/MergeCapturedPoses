88a93a4c173a5e72c4bc8fe204d584c0367115a1
Yukun Guo <gyk.net@gmail.com>
2014-07-26 18:25:30 +0800

    Fix mixed tabs and spaces

--- Program.cs
+++ Program.cs
@@ -11,26 +11,26 @@ namespace MergeCapturedPoses
 		static void Main(string[] args)
 		{
 			string root = @"E:\KinectCapture\RawXML";
-            string[] dirNames = Directory.GetDirectories(root);
-            foreach (var d in dirNames) {
-                string[] fileNames = Directory.GetFiles(d, "*.pose.xml");
-                var writer = new System.IO.StreamWriter(Path.Combine(d, "output.bvh"));
+			string[] dirNames = Directory.GetDirectories(root);
+			foreach (var d in dirNames) {
+				string[] fileNames = Directory.GetFiles(d, "*.pose.xml");
+				var writer = new System.IO.StreamWriter(Path.Combine(d, "output.bvh"));
 
-                writer.Write(BvhTemplate.GetDefaultModel(0.5f, fileNames.Length + 1));
-                foreach (var f in fileNames) {
-                    Dictionary<string, float[]> boneAngleDict = IPIMocapXmlToBvhFrame.GetBoneAngleDict(f);
-                    var t = IPIMocapXmlToBvhFrame.RemapToBvhChannel(boneAngleDict);
-                    foreach (var i in t) {
+				writer.Write(BvhTemplate.GetDefaultModel(0.5f, fileNames.Length + 1));
+				foreach (var f in fileNames) {
+					Dictionary<string, float[]> boneAngleDict = IPIMocapXmlToBvhFrame.GetBoneAngleDict(f);
+					var t = IPIMocapXmlToBvhFrame.RemapToBvhChannel(boneAngleDict);
+					foreach (var i in t) {
 						// prevents scientific notations
 						var str = String.Format("{0:F6}", i);
-                        writer.Write("{0} ", str.TrimEnd("0".ToCharArray()));
-                    }
-                    writer.WriteLine();
-                }
-                writer.Close();
-            }
-            
-            //Console.Read();
+						writer.Write("{0} ", str.TrimEnd("0".ToCharArray()));
+					}
+					writer.WriteLine();
+				}
+				writer.Close();
+			}
+			
+			//Console.Read();
 		}
 	}
 }
